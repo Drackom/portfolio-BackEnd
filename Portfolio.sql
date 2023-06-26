ba45e9b2bfd9d999baa2a5a36ef02b03e38cfac1
Drackom <126530823+Drackom@users.noreply.github.com>
2023-06-26 10:13:17 -0300

    MySQL

---
+++ Portfolio.sql
@@ -0,0 +1,4 @@
+INSERT INTO estudio
+VALUES (1, '2022-2023', 'ArgentinaPrograma - INTI', 'Full Stack Developer Jr',
+'Estudiante del curso Argentina Programa, con la modalidad de primer etapa #SéProgramar y la segunda etapa #YoProgramo.',
+ 1)
