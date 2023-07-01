b44f60ba032f762f71c92dfdf6f5b107eba85aca
Drackom <sergiourielquinteros@gmail.com>
2023-06-30 23:30:04 -0300

    Cors

--- src/main/java/com/miportfolioapp/SpringBoot/WebConfig.java
+++ src/main/java/com/miportfolioapp/SpringBoot/WebConfig.java
@@ -10,19 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @EnableWebMvc
-
-@Configuration
-
+@Configuration 
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
-
     public void addCorsMappings(CorsRegistry registry) {
-
         registry.addMapping("/**");
-
     }
-
-   
-
 }
